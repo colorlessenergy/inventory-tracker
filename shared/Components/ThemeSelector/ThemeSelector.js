bc1b59cc5f7eb4ba4f545819c36fbcb6969c627d
colorlessenergy <brianmunozdev@gmail.com>
2021-08-14 09:16:49 -0700

    add functionality to toggle theme

--- shared/Components/ThemeSelector/ThemeSelector.js
+++ shared/Components/ThemeSelector/ThemeSelector.js
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ThemeSelector () {
     const [ theme, setTheme ] = useState('light');
 
+    useEffect(() => {
+        if (!localStorage.getItem('theme')) {
+            localStorage.setItem('theme', 'light')
+        }
+
+        if (localStorage.getItem('theme') === 'dark') {
+            setTheme('dark');
+        }
+    }, []);
+
+
+    useEffect(() => {
+        if (theme === 'light') {
+            localStorage.setItem('theme', 'light');
+            return document.body.classList.remove('dark');
+        }
+
+        localStorage.setItem('theme', 'dark');
+        return document.body.classList.add('dark');
+    }, [ theme ]);
+
     const toggleTheme = () => {
         setTheme((previousTheme) => {
             if (previousTheme === 'light') {
-                document.body.classList.add('dark');
                 return 'dark';
             }
 
-            document.body.classList.remove('dark');
             return 'light';
         });
     }
