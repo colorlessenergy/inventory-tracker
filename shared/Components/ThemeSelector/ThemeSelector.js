d816f257a89726c4160454883b89f04d58bb1455
colorlessenergy <brianmunozdev@gmail.com>
2021-08-14 09:08:43 -0700

    add functionality to toggle theme

--- shared/Components/ThemeSelector/ThemeSelector.js
+++ shared/Components/ThemeSelector/ThemeSelector.js
@@ -3,8 +3,18 @@ import { useState } from "react";
 export default function ThemeSelector () {
     const [ theme, setTheme ] = useState('light');
 
+    const toggleTheme = () => {
+        setTheme((previousTheme) => {
+            if (previousTheme === 'light') {
+                return 'dark';
+            }
+
+            return 'light';
+        });
+    }
+
     return (
-        <div>
+        <div onClick={ toggleTheme }>
             { theme === 'light' ? ('🌙') : ('☀️') }
         </div>
     );
