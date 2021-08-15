18ebfea21e647d33ec2e61d64fb7a910423258c1
colorlessenergy <brianmunozdev@gmail.com>
2021-08-14 21:19:28 -0700

    add Modal

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
+import { useState } from 'react'
 import Head from 'next/head'
+
+import Modal from '../shared/Components/Modal'
 import ThemeSelector from '../shared/Components/ThemeSelector/ThemeSelector'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>inventory tracker</title>
-        <meta name="description" content="inventory tracker" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [ isModalOpen, setIsModalOpen ] = useState(false);
+    return (
+        <div>
+            <Head>
+                <title>inventory tracker</title>
+                <meta name="description" content="inventory tracker" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <ThemeSelector />
-    </div>
-  )
+            <ThemeSelector />
+
+            <Modal isOpen={ isModalOpen }>
+                <h1>modal</h1>
+            </Modal>
+        </div>
+    )
 }
