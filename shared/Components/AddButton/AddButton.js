fbf43bf4111226c81ee8b4a87373b2b468e1a9d9
colorlessenergy <brianmunozdev@gmail.com>
2021-08-14 21:30:19 -0700

    add button component and styles

---
+++ shared/Components/AddButton/AddButton.js
@@ -0,0 +1,9 @@
+import classes from './AddButton.module.scss';
+
+export default function AddButton () {
+    return (
+        <div className={ classes["add-button"] }>
+            +
+        </div>
+    );
+}
