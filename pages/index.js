e53fcb3463de054f4fd251c831e608a3aae427f9
colorlessenergy <brianmunozdev@gmail.com>
2021-08-14 21:53:48 -0700

    add form elements to add a item block

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,36 @@ export default function Home() {
 
             <AddButton handleClick={ toggleModal } />
             <Modal isOpen={ isModalOpen }>
-                <h1>modal</h1>
+                <form className="flex flex-direction-column">
+                    <label htmlFor="name">
+                        name
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="name"
+                        id="name"
+                        className="mb-1" />
+
+                    <label htmlFor="name">
+                        amount
+                    </label>
+                    <input
+                        type="number"
+                        placeholder="amount"
+                        id="amount"
+                        className="mb-6" />
+
+                    <div className="flex justify-content-between">
+                        <button
+                            type="button"
+                            className="button button-peach">
+                            cancel
+                        </button>
+                        <button className="button button-green">
+                            create
+                        </button>
+                    </div>
+                </form>
             </Modal>
         </div>
     )
