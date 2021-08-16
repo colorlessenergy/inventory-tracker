3b1773d2e75985b6ccc7d04f8f88f03944c778c3
colorlessenergy <brianmunozdev@gmail.com>
2021-08-16 11:51:52 -0700

    remove modal is open class from body in use effect clean up

--- shared/Components/Modal.js
+++ shared/Components/Modal.js
@@ -14,6 +14,10 @@ const Modal = ({ isOpen, toggleModal, children }) => {
         } else {
             document.body.classList.remove('modal-is-open');
         }
+
+        return () => {
+            document.body.classList.remove('modal-is-open');
+        }
     }, [ isOpen ]);
 
     return (
