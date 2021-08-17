1ba663bc1b6c9756cfeb1501f33d9cb90ef934ed
colorlessenergy <brianmunozdev@gmail.com>
2021-08-17 10:31:03 -0700

    add mr-1 class to button

--- shared/Components/FilterItem/FilterItem.js
+++ shared/Components/FilterItem/FilterItem.js
@@ -1,7 +1,7 @@
 export default function FilterItem ({ text, handleClick }) {
     return (
         <button
-            className="button button-white" 
+            className="button button-white mr-1" 
             onClick={ handleClick }>
             { text }
         </button>
